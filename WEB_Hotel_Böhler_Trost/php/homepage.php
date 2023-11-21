131a4d71a6ecb1b6e6e131b2db087e8d5f6fd195
MrPlotTwist <149102011+MrPlotTwist@users.noreply.github.com>
2023-11-21 10:22:24 +0100

    test

--- WEB_Hotel_Böhler_Trost/php/homepage.php
+++ WEB_Hotel_Böhler_Trost/php/homepage.php
@@ -26,15 +26,8 @@ session_start();
           background-size: 100% 100%;
         }
         </style>
-        <nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-      Bootstrap
-    </a>
-  </div>
-</nav>
-<!--<nav class="navbar navbar-expand-lg bg-body-tertiary">
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="http://localhost/webtechnologie/Semesterprojekt/Web_Hotel_Project/WEB_Hotel_Böhler_Trost/php/homepage.php"><img src="http://localhost/webtechnologie/Semesterprojekt/Web_Hotel_Project/WEB_Hotel_Böhler_Trost/res/img/Logoneu.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-top">
       Home
@@ -61,7 +54,7 @@ session_start();
       </ul>
     </div>
   </div>
-</nav> -->
+</nav>
     <div class="row w-100 p-3 mx-auto p-2" style="width: 120px;">
     <div class="col-sm">
       <div class="card">
