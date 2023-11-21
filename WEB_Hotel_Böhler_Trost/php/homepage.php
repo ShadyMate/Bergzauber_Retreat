d430619390bb84895c39da76254e2bb9dfb8a50e
ShadyMate <luisboehler@proton.me>
2023-11-21 14:47:02 +0100

    homepage.php font update, h1 update

--- WEB_Hotel_Böhler_Trost/php/homepage.php
+++ WEB_Hotel_Böhler_Trost/php/homepage.php
@@ -11,11 +11,11 @@ session_start();
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital@1&family=Press+Start+2P&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../css/stylesheet.css">
+    <link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <header>
-    <h1 style="color: #FFFFFF" class="hotelname">Bergzauber Retreat</h1>
+    <h1 class="hotelname">Bergzauber Retreat</h1>
 </header>
 <body>
 
