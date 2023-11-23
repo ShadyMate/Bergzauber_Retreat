f2b644cc07a7771b74970d4e6c27eb9211b25df1
ShadyMate <luisboehler@proton.me>
2023-11-23 18:19:49 +0100

    news.php looks a bit better

--- WEB_Hotel_Böhler_Trost/includes/news.php
+++ WEB_Hotel_Böhler_Trost/includes/news.php
@@ -73,8 +73,8 @@ if ($uploadOk == 0) {
     </form>';
   }
     ?>
-<main class="main container bildrand">
-            <article>
+<main class="main container">
+            <form>
                 <div id="carouselExample" class="carousel slide">
                     <div class="carousel-inner">
                         <?php
@@ -99,7 +99,7 @@ if ($uploadOk == 0) {
                       <span class="visually-hidden">Next</span>
                     </button>
                   </div>
-            </article>
+            </form>
         </main>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
