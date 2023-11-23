268e22636625eec654f2a9ac72a72a9540160332
ShadyMate <luisboehler@proton.me>
2023-11-23 15:41:10 +0100

    I redid the cards yipieee

--- WEB_Hotel_Böhler_Trost/includes/cards.php
+++ WEB_Hotel_Böhler_Trost/includes/cards.php
@@ -1,30 +1,25 @@
-<div class="row row-cols-1 row-cols-md-3 g-4" style="width: 95%; margin: 0 auto">
-<div class="col" style="padding-top: 20px;">
-  <div class="card h-100">
-    <img src="../res/img/hotelzimmer.jpg" class="card-img-top" alt="Hotelzimmer">
-    <div class="card-body">
-      <h5 class="card-title">Zimmer Buchen.</h5>
-      <p class="card-text">Hier finden Sie alle Angebote, sowie Infos zur Buchung</p>
-      <a href="../includes/Zimmer.php" class="btn btn-primary">Zimmer buchen</a>
+<div class="card-group">
+    <div class="card">
+        <img class="card-img-top" src="../res/img/hotelzimmer.jpg" alt="Hotelzimmer">
+        <div class="card-body">
+            <h5 class="card-title">Zimmerangebot</h5>
+            <p class="card-text">Hier finden Sie alle Angebote, sowie Infos zur Buchung</p>
+            <a href="../includes/Zimmer.php" class="btn btn-primary">Zimmer buchen</a>
+        </div>
     </div>
-  </div>
-</div>
-<div class="col" style="padding-top: 20px">
-  <div class="card h-100">
-    <img src="../res/img/radfahren.jpg" class="card-img-top" alt="Radweg">
-    <div class="card-body">
-      <h5 class="card-title">Freizeitaktivitäten</h5>
-      <p class="card-text">In unsere Gegend gibt es viel zu sehen! Hier finden Sie unsere Empfehlungen.</p>
+    <div class="card">
+        <img class="card-img-top" src="../res/img/radfahren.jpg" alt="Radweg">
+        <div class="card-body">
+            <h5 class="card-title">Aktivitäten</h5>
+            <p class="card-text">Rund um das Hotel gibt es viele Dinge zu tun und zu sehen.
+                Hier finden sie einige beliebte Aktivitäten</p>
+        </div>
     </div>
-  </div>
-</div>
-<div class="col" style="padding-top: 20px">
-  <div class="card h-100">
-    <img src="../res/img/SpaImage.jpeg" class="card-img-top" alt="Spa">
-    <div class="card-body">
-      <h5 class="card-title">Unser Spa-Bereich</h5>
-      <p class="card-text">Hier können Sie eine erholsame Zeit verbringen.</p>
+    <div class="card">
+        <img class="card-img-top" src="../res/img/SpaImage.jpeg" alt="Spa">
+        <div class="card-body">
+            <h5 class="card-title">Unser Spa Angebot</h5>
+            <p class="card-text">Wir haben eine große Auswahl an Spa Angeboten für sie.</p>
+        </div>
     </div>
-  </div>
-</div>
 </div>
